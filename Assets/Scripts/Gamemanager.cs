ee6b4388dc3d6ffbe10ff2493cc60b9c07f91ccc
ycMia <zzlyx20123201@126.com>
2020-12-05 00:05:21 +0800

    添加了基础的人物移动, 跳跃

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class Gamemanager : MonoBehaviour
 {
+    public Camera mCCamera; //MainCharacterCamera
+    public GameObject mC; //MainCharacter
+    private Rigidbody2D mCR2D;
+    public int g_jumpEnabledCount = 2;
+    public int g_jumpEnabledLimit = 2;
+
+
+    public float g_accelerationX;
+    public float g_VelocityJump;
+    public float g_restrictVelocityXAbs;
+    public float g_restrictVelocityYAbs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mCR2D = mC.GetComponent<Rigidbody2D>();
     }
+    
+    public bool AddJump()
+    {
+        if ( g_jumpEnabledCount < g_jumpEnabledLimit)
+        {
+            g_jumpEnabledCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public void SetCharacterSlideAtSide(Rigidbody2D rigidbody2D, int mode) 
+        // 1 : notAllowRightwayMovement
+        // 2 : notAllowLeftwayMovement
+    {
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Camera
+        mCCamera.transform.position = Vector3.Lerp(mCCamera.transform.position, mC.transform.position, 0.1f);
+        mCCamera.transform.position = new Vector3(mCCamera.transform.position.x, mCCamera.transform.position.y,  - 10f);
+        //Movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            mCR2D.velocity += new Vector2(-g_accelerationX, 0);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            mCR2D.velocity += new Vector2(g_accelerationX, 0 );
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(g_jumpEnabledCount > 0)
+            {
+                mCR2D.velocity = new Vector2(mCR2D.velocity.x , g_VelocityJump);
+                g_jumpEnabledCount--;
+            }
+        }
+        //Final Fixing
+        RestrictMaxSpeed(mCR2D, g_restrictVelocityXAbs, g_restrictVelocityYAbs);
+    }
+    
+    
+
+    void RestrictMaxSpeed(Rigidbody2D rigidbody2D,float restrictXAbs,float restrictYAbs)
+    {
+        if(Mathf.Abs(rigidbody2D.velocity.x) >= restrictXAbs)
+        {
+            if(rigidbody2D.velocity.x > 0)
+                rigidbody2D.velocity = new Vector2(restrictXAbs, rigidbody2D.velocity.y);
+            else
+                rigidbody2D.velocity = new Vector2(-restrictXAbs, rigidbody2D.velocity.y);
+        }
+        if (Mathf.Abs(rigidbody2D.velocity.y) >= restrictYAbs) //attention here because of its 
+        {
+            if (rigidbody2D.velocity.y > 0)
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, restrictYAbs);
+            else
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, -restrictYAbs);
+        }
     }
 }
---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    public Gamemanager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ( collision.transform.gameObject.tag == "Ground" )
+        {
+            if (gm.AddJump() == true)
+            {
+                gm.AddJump();
+            }   
+            else
+            {
+
+            }
+            // two times if touched ground
+            // and a bit fixing
+        }
+
+        else if(collision.transform.gameObject.tag == "Ground_Side_Left")
+        {
+            
+        }
+
+        else if (collision.transform.gameObject.tag == "Ground_Side_Right")
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
